a6c95e2436a884b55cb57e3c17f86c20733cc886
King-of-rudwick <nddibathia@gmail.com>
2024-07-08 04:36:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -69,14 +69,14 @@ module.exports = {
 
   menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
-  HANDLERS: process.env.PREFIX  || ".",
+  HANDLERS: process.env.PREFIX  || "€",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.8",
   caption : global.caption || "" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
  
-  author : process.env.PACK_AUTHER|| "",
-  packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
+  author : process.env.PACK_AUTHER|| "KING OF RUDWICK",
+  packname: process.env.PACK_NAME || "FOOL",
+  botname : process.env.BOT_NAME  || "AKANE",
   ownername:process.env.OWNER_NAME|| "Suhail",
 
 
